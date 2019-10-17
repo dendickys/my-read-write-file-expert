5ff7c52d10cd647d75e7674656c620000ef1e247
dendickys <febri.unquad2014@gmail.com>
2019-10-18 06:03:43 +0700

    Fix ReadWrite

---
+++ app/src/main/java/com/example/myreadwritefileexpert/FileHelper.java
@@ -0,0 +1,54 @@
+package com.example.myreadwritefileexpert;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class FileHelper {
+    private static final String TAG = FileHelper.class.getName();
+
+    static void writeToFile(FileModel fileModel, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileModel.getFilename(), Context.MODE_PRIVATE));
+            outputStreamWriter.write(fileModel.getData());
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e(TAG, "File write failed: ", e);
+        }
+    }
+
+    static FileModel readFromFile(Context context, String filename) {
+        FileModel fileModel = new FileModel();
+
+        try {
+            InputStream inputStream = context.openFileInput(filename);
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                fileModel.setData(stringBuilder.toString());
+                fileModel.setFilename(filename);
+            }
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found : ", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Can;t read file : ", e);
+        }
+
+        return fileModel;
+    }
+}
